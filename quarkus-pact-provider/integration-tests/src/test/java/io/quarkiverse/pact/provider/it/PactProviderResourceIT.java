92def98affa4fdc7d88bcfe6c9affc272f92d4a8
Holly Cummins <hcummins@redhat.com>
2022-10-19 18:15:30 +0100

    Output of create-extension Maven Mojo
    Skeleton code for the quarkus-pact-provider extension.

---
+++ quarkus-pact-provider/integration-tests/src/test/java/io/quarkiverse/pact/provider/it/PactProviderResourceIT.java
@@ -0,0 +1,7 @@
+package io.quarkiverse.pact.provider.it;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class PactProviderResourceIT extends PactProviderResourceTest {
+}
