fce1e503bce6da848792056a8aee5a740591d3ef
Holly Cummins <hcummins@redhat.com>
2024-02-09 12:19:07 +0000

    Update minimum extension version to Quarkus 3.7.0 ad increase amount of memory available to tests

--- cross-extension-integration-tests/src/test/java/io/quarkiverse/pact/it/DevModeContractTestIT.java
+++ cross-extension-integration-tests/src/test/java/io/quarkiverse/pact/it/DevModeContractTestIT.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.maven.shared.invoker.MavenInvocationException;
@@ -13,6 +16,8 @@ import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
 import io.quarkus.maven.it.RunAndCheckMojoTestBase;
 import io.quarkus.maven.it.continuoustesting.ContinuousTestingMavenTestUtils;
+import io.quarkus.maven.it.verifier.RunningInvoker;
+import io.quarkus.runtime.LaunchMode;
 
 /**
  * Because Pact uses Kotlin under the covers, we see different behaviour
@@ -25,6 +30,52 @@ import io.quarkus.maven.it.continuoustesting.ContinuousTestingMavenTestUtils;
  */
 @DisabledIfSystemProperty(named = "quarkus.test.native", matches = "true")
 public class DevModeContractTestIT extends RunAndCheckMojoTestBase {
+
+    private static final int DEFAULT_PORT = 8080;
+
+    // See https://github.com/quarkiverse/quarkus-pact/issues/180
+    // These tests OOM with Quarkus 3.7+, so we need to bump the amount of memory they have
+    protected void run(boolean performCompile, LaunchMode mode, boolean skipAnalytics, String... options)
+            throws FileNotFoundException, MavenInvocationException {
+        assertThat(testDir).isDirectory();
+        running = new RunningInvoker(testDir, false);
+
+        final List<String> args = new ArrayList<>(3 + options.length);
+        if (performCompile) {
+            args.add("compile");
+        }
+        args.add("quarkus:" + mode.getDefaultProfile());
+        if (skipAnalytics) {
+            args.add("-Dquarkus.analytics.disabled=true");
+        }
+
+        // If the test has set a different port, pass that on to the application
+        if (getPort() != DEFAULT_PORT) {
+            int port = getPort();
+            int testPort = getPort() + 1;
+            args.add("-Dquarkus.http.port=" + port);
+            args.add("-Dquarkus.http.test-port=" + testPort);
+        }
+
+        boolean hasDebugOptions = false;
+        for (String option : options) {
+            args.add(option);
+            if (option.trim().startsWith("-Ddebug=") || option.trim().startsWith("-Dsuspend=")) {
+                hasDebugOptions = true;
+            }
+        }
+        if (!hasDebugOptions) {
+            // if no explicit debug options have been specified, let's just disable debugging
+            args.add("-Ddebug=false");
+        }
+
+        //we need to limit the memory consumption, as we can have a lot of these processes
+        //running at once, if they add default to 75% of total mem we can easily run out
+        //of physical memory as they will consume way more than what they need instead of
+        //just running GC
+        args.add("-Djvm.args='-Xmx140m'");
+        running.execute(args, Map.of());
+    }
 
     protected void runAndCheck(String... options) throws MavenInvocationException, FileNotFoundException {
         // To avoid clashes with other tests, we use a non-default port
